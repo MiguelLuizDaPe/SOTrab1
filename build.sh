eaeaec459f65228ce856a3237ea5f05e743aab0d
miguel <drporrola@gmail.com>
2023-09-18 09:55:15 -0300

    arrumei algumas variáveis no da matriz

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cflags="-std=c++17 -pipe -O2 -Wall -Wextra -fno-strict-aliasing -L. -I. -pthread
 #2> /dev/null é pra não ler o que o imprimirMatriz ta imprimindo pra fingir que ta usando o matriz pra não da uma otimização que faz o programa dar errado
 # $cc $cflags  main-bubble.cpp -o main-bb.bin && ./main-bb.bin $@ 2> /dev/null
 $cc $cflags main-matriz.cpp -o main-mt.bin && ./main-mt.bin 2> /dev/null
+# $cc $cflags main-matriz-proc.cpp -o main-mt-proc.bin && ./main-mt-proc.bin 2> /dev/null
--- main-matriz-proc.cpp
+++ main-matriz-proc.cpp
@@ -1,9 +1,12 @@
 #include <bits/chrono.h>
 #include <chrono>
+#include <cstddef>
+#include <ctime>
 #include <iostream>
 #include <memory>
 #include <random>
-#include <thread>
+#include <unistd.h>
+// #include <thread>
 
 #include "matriz.hpp"
 #include "types.hpp"
@@ -75,53 +78,60 @@ int main() {
 
   constexpr usize M = 5000;//tamanho da lateral da matriz
 
-  // botado no heap pq as thread compartilha memória no heap
-  //make_unique é pra se mata no final do escopo
-  auto a = std::make_unique<Matriz<M>>();//cria as matriz que vão ser acessadas
-  auto b = std::make_unique<Matriz<M>>();
+  Matriz<M> *a = mmap(NULL, sizeof(Matriz<M>), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+  Matriz<M> *b = mmap(NULL, sizeof(Matriz<M>), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+
+  // auto a = std::make_unique<Matriz<M>>();//cria as matriz que vão ser acessadas
+  // auto b = std::make_unique<Matriz<M>>();
 
   preencherMatriz(*a);//preenche elas
   preencherMatriz(*b);
 
-  auto multiPosSingle = std::make_unique<Matriz<M>>();//cria as matrizes as quais vão ser botadas os valores
+  Matriz<M> multiPosSingle;//cria as matrizes as quais vão ser botadas os valores
   Matriz<M> multiPosMulti; 
-  auto multiMatSingle = std::make_unique<Matriz<M>>();
+  Matriz<M> multiMatSingle;
   Matriz<M> multiMatMulti;
 
-  auto elapsedSecondsPosSingle = std::make_unique<chrono::nanoseconds>();//os tempos totais de cada multiplicação
+  chrono::nanoseconds elapsedSecondsPosSingle;//os tempos totais de cada multiplicação
   chrono::nanoseconds elapsedSecondsPosMulti;
-  auto elapsedSecondsMatSingle = std::make_unique<chrono::nanoseconds>();
+  chrono::nanoseconds elapsedSecondsMatSingle;
   chrono::nanoseconds elapsedSecondsMatMulti;
+
+  pid_t pid = fork();
 
   //ta em uma thread e o outro não pq o multithread ta utilizando a thread do main(a thread do processo)
   //assim a thread do main não fica em repouso fazendo nada esperando as outras thread terminarem terminarem
-  auto t1 = std::thread([&]() {
+  if(pid > 0) {
     const auto startPosSingle{chrono::steady_clock::now()};//começa a contar daqui
-    *multiPosSingle = multilpicaMatrizPosicional(*a, *b);//essa função ta vindo do matriz.hpp
+    multiPosSingle = multilpicaMatrizPosicional(*a, *b);//essa função ta vindo do matriz.hpp
     const auto endPosSingle{chrono::steady_clock::now()};//e vai até aqui, só pega o tempo de execução da função(não vou repetir pros outros)
-    *elapsedSecondsPosSingle = chrono::duration_cast<chrono::nanoseconds>(endPosSingle - startPosSingle);//calcula o tempo total
-  });
+    elapsedSecondsPosSingle = chrono::duration_cast<chrono::nanoseconds>(endPosSingle - startPosSingle);//calcula o tempo total
+  }
 
-  const auto startPosMulti{chrono::steady_clock::now()};
-  multiPosMulti = posMultiThr(*a, *b);
-  const auto endPosMulti{chrono::steady_clock::now()};
-  elapsedSecondsPosMulti = chrono::duration_cast<chrono::nanoseconds>(endPosMulti - startPosMulti);
+  else{
+    const auto startPosMulti{chrono::steady_clock::now()};
+    multiPosMulti = posMultiThr(*a, *b);
+    const auto endPosMulti{chrono::steady_clock::now()};
+    elapsedSecondsPosMulti = chrono::duration_cast<chrono::nanoseconds>(endPosMulti - startPosMulti);
+  }
 
-  t1.join();//espera a thread t1 terminar pra liberar o resto
+//   t1.join();//espera a thread t1 terminar pra liberar o resto
 
-  auto t2 = std::thread([&](){
+  if(pid > 0){
     const auto startMatSingle{std::chrono::steady_clock::now()};
     *multiMatSingle = multilpicaMatrizMatricial(*a, *b);//essa função ta vindo do matriz.hpp
     const auto endMatSingle{std::chrono::steady_clock::now()};//escolhi nanosegundo pq em segundo e outros tipos de conversão ficam no estilo 1e-16 e é feio
     *elapsedSecondsMatSingle = chrono::duration_cast<chrono::nanoseconds>(endMatSingle - startMatSingle);
-  });
+  }
 
-  const auto startMatMulti{std::chrono::steady_clock::now()};
-  multiMatMulti = matMultiThr(*a, *b);
-  const auto endMatMulti{std::chrono::steady_clock::now()};
-  elapsedSecondsMatMulti = chrono::duration_cast<chrono::nanoseconds>(endMatMulti - startMatMulti);
+  else{
+    const auto startMatMulti{std::chrono::steady_clock::now()};
+    *multiMatMulti = matMultiThr(*a, *b);
+    const auto endMatMulti{std::chrono::steady_clock::now()};
+    *elapsedSecondsMatMulti = chrono::duration_cast<chrono::nanoseconds>(endMatMulti - startMatMulti);
+  }
 
-  t2.join();
+  // t2.join();
 
 
   std::cout << "Posicional Sigle\n";
@@ -130,7 +140,7 @@ int main() {
 
   std::cout << "Posicional Multi\n";//se o matriz for pequena a multithread fica uma porra pq custa criar thread, esse custo fica sombreado quando a matriz fica grandona
   // imprimeMatriz(*multiPosMulti);
-  std::cout << elapsedSecondsPosMulti.count() << "\n";
+  std::cout << elapsedSecondsPosMulti->count() << "\n";
 
 
   std::cout << "Matricial Sigle\n";
@@ -139,7 +149,7 @@ int main() {
 
   std::cout << "Matricial Multi\n";
   // imprimeMatriz(*multiMatMulti);
-  std::cout << elapsedSecondsMatMulti.count() << "\n";
+  std::cout << elapsedSecondsMatMulti->count() << "\n";
 
   return 0;
 }
