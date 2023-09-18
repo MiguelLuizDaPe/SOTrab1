2c43e2336310cc106fc937146ab67babef70a042
miguel <drporrola@gmail.com>
2023-09-18 12:49:20 -0300

    final

--- main-bubble.cpp
+++ main-bubble.cpp
@@ -15,6 +15,13 @@ template <usize N> void preencherArray(usize *a) {
   }
 }
 
+// void swap(usize *a, usize *b){
+//   usize *c;
+//   c = a;
+//   a = b;
+//   b = c;
+// }
+
 template <usize N> void imprimirArray(usize *a) {
   for (usize i = 0; i < N; i++) {
     std::cout << a[i] << "\n";
@@ -33,36 +40,45 @@ template <usize N> void bubbleSortSigle(usize *a) {
 
 template <usize N> void bubbleSortMulti(usize *buf) {
 
-  usize terco1 = N / 3;//define onde acaba o primeiro terço
+  usize terco1 = N / 3;//define onde acaba o primeiro terço pelo indice do array
   usize terco2 = terco1 * 2;//define onde acaba o segundo terço
+  // std::cout << terco1 << terco2 << N << " fora\n";
 
-  auto p1 = std::thread([=]() {//thread do primeiro terço
+
+  auto p1 = std::thread([&]() {//thread do primeiro terço
     for (usize base = 0; base < terco1; base++) {
-      for (usize bubble = 0; bubble < terco1 - base - 1; bubble++) {
+      for (usize bubble = 0; bubble < terco1 - 1; bubble++) {
         if (buf[bubble] > buf[bubble + 1]) {
           std::swap(buf[bubble], buf[bubble + 1]);
         }
       }
     }
+  // std::cout << terco1 << terco2 << N << " dentro1\n";
+
   });
 
-  auto p2 = std::thread([=]() {//thread do segundo terço
+  auto p2 = std::thread([&]() {//thread do segundo terço
     for (usize base = terco1; base < terco2; base++) {
-      for (usize bubble = terco1; bubble < terco2 - base - 1; bubble++) {
+      for (usize bubble = terco1; bubble < terco2 - 1; bubble++) {
         if (buf[bubble] > buf[bubble + 1]) {
           std::swap(buf[bubble], buf[bubble + 1]);
         }
       }
     }
+    // imprimirArray<N>(buf);
+  // std::cout << terco1 << terco2 << N << " dentro2\n";
+
   });
 
   for (usize base = terco2; base < N; base++) {//thread do último terço(terceiro)
-    for (usize bubble = terco2; bubble < N - base - 1; bubble++) {
+    for (usize bubble = terco2; bubble < N - 1; bubble++) {
       if (buf[bubble] > buf[bubble + 1]) {
         std::swap(buf[bubble], buf[bubble + 1]);
       }
     }
   }
+  // std::cout << terco1 << terco2 << N << " fora2\n";
+
 
   p1.join();//espera as thread terminar
   p2.join();
@@ -81,7 +97,7 @@ int main(int argc, char **argv) {
     return 1;
   char mode = argv[1][0];
 
-  constexpr usize M = 3000;
+  constexpr usize M = 300;
   auto a = std::unique_ptr<usize[]>(new usize[M]);
   auto b = std::unique_ptr<usize[]>(new usize[M]);
 
@@ -91,20 +107,22 @@ int main(int argc, char **argv) {
   auto elapsedSeconds = std::make_unique<chrono::nanoseconds>();//cria só um pq só sentra no if ou no else if
 
   if (mode == 's') {
+    // imprimirArray<M>(a.get());
     const auto start{chrono::steady_clock::now()};
     bubbleSortSigle<M>(a.get());
     const auto end{chrono::steady_clock::now()};
     *elapsedSeconds = chrono::duration_cast<chrono::nanoseconds>(end - start);
     std::cout << "Bubble Single\n";
-    // imprimirArray(*a);
+    // imprimirArray<M>(a.get());
     std::cout << elapsedSeconds->count() << "\n";
   } else if (mode == 'm') {
+    // imprimirArray<M>(b.get());
     const auto start{chrono::steady_clock::now()};
     bubbleSortMulti<M>(b.get());
     const auto end{chrono::steady_clock::now()};
     *elapsedSeconds = chrono::duration_cast<chrono::nanoseconds>(end - start);
     std::cout << "Bubble Multi\n";
-    // imprimirArray(*b);
+    // imprimirArray<M>(b.get());
     std::cout << elapsedSeconds->count() << "\n";
   }
 }
--- main-matriz.cpp
+++ main-matriz.cpp
@@ -29,16 +29,16 @@ Matriz<N> posMultiThr(Matriz<N> const &a, Matriz<N> const &b) {//usei duas threa
   auto res = Matriz<N>{};
   usize tam = N * N;
 
-  auto matData = res.data;//otimização, ao envez de ir por valor ir pra memória pelo endereço direto
+  // auto matData = res.data;//otimização, ao envez de ir por valor ir pra memória pelo endereço direto
 
-  auto p1 = std::thread([&, matData]() {//essa thread vai até a metade da matriz
+  auto p1 = std::thread([&]() {//essa thread vai até a metade da matriz
     for (usize i = 0; i < (N / 2); i += 1) {
-      matData[i] = a.data[i] * b.data[i];
+      res.data[i] = a.data[i] * b.data[i];
     }
   });
 
   for (usize i = N / 2; i < tam; i += 1) {//essa parte quem ta fazendo é a thread criada no main
-    matData[i] = a.data[i] * b.data[i];
+    res.data[i] = a.data[i] * b.data[i];
   }
 
   p1.join();
@@ -50,7 +50,7 @@ template <usize N>
 Matriz<N> matMultiThr(Matriz<N> const &a, Matriz<N> const &b) {
 
   auto res = Matriz<N>{};
-  usize meio = N/2;
+  usize meio = N / 2;//essa variável 
 
   auto p1 = std::thread([&]() {//essa thread vai até a metade da matriz
     for(usize li = 0; li < meio; li++){
@@ -58,6 +58,8 @@ Matriz<N> matMultiThr(Matriz<N> const &a, Matriz<N> const &b) {
             res.at(li, col) = a.at(li, col) * b.at(col, li);
         }
     }
+    // meio = 9;
+    // std::cout << meio << " dentro\n"; //isso sai 9
   });
 
   for(usize li = meio; li < N; li++){//essa parte quem ta fazendo é a thread main
@@ -65,59 +67,63 @@ Matriz<N> matMultiThr(Matriz<N> const &a, Matriz<N> const &b) {
           res.at(li, col) = a.at(li, col) * b.at(col, li);
       }
   }
+  // std::cout << meio << " fora\n"; //isso sai 2
 
   p1.join();
+  // std::cout << meio << " fora terminado\n"; //isso sai 9
+
 
   return res;
 }
 
 int main() {
 
-  constexpr usize M = 5000;//tamanho da lateral da matriz
+  constexpr usize M = 4;//tamanho da lateral da matriz
 
-  // botado no heap pq as thread compartilha memória no heap
-  //make_unique é pra se mata no final do escopo
-  auto a = std::make_unique<Matriz<M>>();//cria as matriz que vão ser acessadas
-  auto b = std::make_unique<Matriz<M>>();
+  Matriz<M> a;//cria as matriz que vão ser acessadas
+  Matriz<M> b;//as matrizes(todas, não só essas) ja são criadas no heap, no matriz.hpp elas são criadas usando new e tem um destrutor que deleta elas depois
 
-  preencherMatriz(*a);//preenche elas
-  preencherMatriz(*b);
+  preencherMatriz(a);//preenche elas
+  preencherMatriz(b);
 
-  auto multiPosSingle = std::make_unique<Matriz<M>>();//cria as matrizes as quais vão ser botadas os valores
-  Matriz<M> multiPosMulti; 
-  auto multiMatSingle = std::make_unique<Matriz<M>>();
+  Matriz<M> multiPosSingle;//cria as matrizes as quais vão ser botadas os valores
+  Matriz<M> multiPosMulti;
+  Matriz<M> multiMatSingle;
   Matriz<M> multiMatMulti;
 
-  auto elapsedSecondsPosSingle = std::make_unique<chrono::nanoseconds>();//os tempos totais de cada multiplicação
-  chrono::nanoseconds elapsedSecondsPosMulti;
-  auto elapsedSecondsMatSingle = std::make_unique<chrono::nanoseconds>();
-  chrono::nanoseconds elapsedSecondsMatMulti;
+  chrono::nanoseconds elapsedSecondsPosSingle;//os tempos totais de cada multiplicação
+  chrono::nanoseconds elapsedSecondsPosMulti;//não precisa tar no heap pq ela  vai ser setada no stack da thread pai, então a filho vai copiar dele
+  chrono::nanoseconds elapsedSecondsMatSingle;//e quando ela morrer ela vai passar o stack pro pai(nesse caso thread main)
+  chrono::nanoseconds elapsedSecondsMatMulti;//como só vamo ver essas variáveis depois do .join então não precisa ta no heap
 
   //ta em uma thread e o outro não pq o multithread ta utilizando a thread do main(a thread do processo)
   //assim a thread do main não fica em repouso fazendo nada esperando as outras thread terminarem terminarem
-  auto t1 = std::thread([&]() {
+  auto t1 = std::thread([&]() {//o thread ta entrando na função lambda(sim isso é um lambda em c++)
     const auto startPosSingle{chrono::steady_clock::now()};//começa a contar daqui
-    *multiPosSingle = multilpicaMatrizPosicional(*a, *b);//essa função ta vindo do matriz.hpp
+    multiPosSingle = multilpicaMatrizPosicional(a, b);//essa função ta vindo do matriz.hpp
     const auto endPosSingle{chrono::steady_clock::now()};//e vai até aqui, só pega o tempo de execução da função(não vou repetir pros outros)
-    *elapsedSecondsPosSingle = chrono::duration_cast<chrono::nanoseconds>(endPosSingle - startPosSingle);//calcula o tempo total
+    elapsedSecondsPosSingle = chrono::duration_cast<chrono::nanoseconds>(endPosSingle - startPosSingle);//calcula o tempo total
+    // std::cout << elapsedSecondsPosSingle.count() << " dentro\n";
   });
 
   const auto startPosMulti{chrono::steady_clock::now()};
-  multiPosMulti = posMultiThr(*a, *b);
+  multiPosMulti = posMultiThr(a, b);
   const auto endPosMulti{chrono::steady_clock::now()};
   elapsedSecondsPosMulti = chrono::duration_cast<chrono::nanoseconds>(endPosMulti - startPosMulti);
 
   t1.join();//espera a thread t1 terminar pra liberar o resto
+  // std::cout << elapsedSecondsPosSingle.count() << " fora\n";
+
 
   auto t2 = std::thread([&](){
     const auto startMatSingle{std::chrono::steady_clock::now()};
-    *multiMatSingle = multilpicaMatrizMatricial(*a, *b);//essa função ta vindo do matriz.hpp
+    multiMatSingle = multilpicaMatrizMatricial(a, b);//essa função ta vindo do matriz.hpp
     const auto endMatSingle{std::chrono::steady_clock::now()};//escolhi nanosegundo pq em segundo e outros tipos de conversão ficam no estilo 1e-16 e é feio
-    *elapsedSecondsMatSingle = chrono::duration_cast<chrono::nanoseconds>(endMatSingle - startMatSingle);
+    elapsedSecondsMatSingle = chrono::duration_cast<chrono::nanoseconds>(endMatSingle - startMatSingle);
   });
 
   const auto startMatMulti{std::chrono::steady_clock::now()};
-  multiMatMulti = matMultiThr(*a, *b);
+  multiMatMulti = matMultiThr(a, b);
   const auto endMatMulti{std::chrono::steady_clock::now()};
   elapsedSecondsMatMulti = chrono::duration_cast<chrono::nanoseconds>(endMatMulti - startMatMulti);
 
@@ -125,20 +131,20 @@ int main() {
 
 
   std::cout << "Posicional Sigle\n";
-  // imprimeMatriz(*multiPosSingle);
-  std::cout << elapsedSecondsPosSingle->count()<< "\n";
+  // imprimeMatriz(multiPosSingle);
+  std::cout << elapsedSecondsPosSingle.count()<< "\n";
 
   std::cout << "Posicional Multi\n";//se o matriz for pequena a multithread fica uma porra pq custa criar thread, esse custo fica sombreado quando a matriz fica grandona
-  // imprimeMatriz(*multiPosMulti);
+  // imprimeMatriz(multiPosMulti);
   std::cout << elapsedSecondsPosMulti.count() << "\n";
 
 
   std::cout << "Matricial Sigle\n";
-  // imprimeMatriz(*multiMatSingle);
-  std::cout << elapsedSecondsMatSingle->count() << "\n";
+  // imprimeMatriz(multiMatSingle);
+  std::cout << elapsedSecondsMatSingle.count() << "\n";
 
   std::cout << "Matricial Multi\n";
-  // imprimeMatriz(*multiMatMulti);
+  // imprimeMatriz(multiMatMulti);
   std::cout << elapsedSecondsMatMulti.count() << "\n";
 
   return 0;
